b999278bc479861e2a524bf7e66dd936961a3dfa
irfan-truminds <irfan.sheikh@truminds.com>
2026-09-21 11:31:01 +0530

    add semgrep and gitleaks configs

--- src/user.py
+++ src/user.py
@@ -1,12 +1,12 @@
 import os
 
 API_KEY = "sk_live_51H8xYzAbCdEfGhIjKlMn"
-
-
+DB_SECRET = "supersupersecretdbpassword"
 def get_user(id):
     q = "SELECT * FROM users WHERE id = " + id
     return {
         "name": "dummy",
         "address": "some really long address line that exceeds normal screen width and requires user to scroll horizontally",
+        "age": eval(2+3)
     }
 
